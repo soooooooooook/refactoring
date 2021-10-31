83c60d3f2b22edda1c34b05a4fa1b40595fbf0ef
이선숙 <seonsook.lee@twolinecode.com>
2021-10-31 22:53:17 +0900

    56p

--- src/statement.js
+++ src/statement.js
@@ -1,70 +1,85 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0;
-  let result = `청구 내역(고객명: ${invoice.customer}\n`;
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    return renderPlainText(statementData, plays);
 
-  for (let perf of invoice.performances) {
-
-    // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    totalAmount += amountFor(perf);
-  }
-  result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점`;
-  return result;
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (playFor(aPerformance).type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
+        return result;
     }
-    return result;
-  }
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID]
-  }
-
-  function volumeCreditFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === playFor(aPerformance).type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
-  function usd(aNumber) {
-    return  new Intl.NumberFormat('en-Us', {
-      style: 'currency', currency: 'USD', minimumFractionDigits: 2
-    }).format(aNumber/100);
-  }
-
-  function totalVolumeCredits() {
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditFor(perf)
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID]
     }
-    return volumeCredits;
-  }
+}
 
-  function appleSouce() {
-    let totalAmount = 0;
+function renderPlainText(data, plays) {
+    let result = `청구 내역(고객명: ${data.customer}\n`;
+    for (let perf of data.performances) {
+        result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+    }
+    result += `총액: ${usd(totalAmount())}\n`;
+    result += `적립 포인트: ${totalVolumeCredits()}점`;
+    return result;
 
-  }
+    function amountFor(aPerformance) {
+        let result = 0;
+
+        switch (playFor(aPerformance).type) {
+            case 'tragedy':
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case 'comedy':
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        }
+        return result;
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID]
+    }
+
+    function volumeCreditFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ('comedy' === playFor(aPerformance).type)
+            result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat('en-Us', {
+            style: 'currency', currency: 'USD', minimumFractionDigits: 2
+        }).format(aNumber / 100);
+    }
+
+    function totalVolumeCredits() {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += volumeCreditFor(perf)
+        }
+        return result;
+    }
+
+    function totalAmount() {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += amountFor(perf);
+        }
+        return result;
+    }
 }
 
 
